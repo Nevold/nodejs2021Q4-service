7b43051f0c72fb2df95368667dd1b2b3ad48f553
Nevold <nevold@tut.by>
2021-12-23 21:37:05 +0300

    feat: add basic loggin

--- src/resources/db/db.ts
+++ src/resources/db/db.ts
@@ -21,7 +21,20 @@ interface Repo {
   }>;
 }
 export const items: Repo = {
-  user: [],
+  user: [
+    {
+      id: '17af78ee-3c30-4ec1-a849-53907e37199d',
+      name: 'TEST_USER',
+      login: 'test_user',
+      password: '1T35t_P@55w0rd',
+    },
+    {
+      id: '927f5a59-88f7-44f5-8c06-d8c0330ca5fc',
+      name: 'TEST_USER2',
+      login: 'test_user2',
+      password: '2T35t_P@55w0rd',
+    },
+  ],
   board: [],
   task: [],
 };
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,40 @@
 import fastify, { FastifyPluginOptions } from 'fastify';
+import pino from 'pino';
 import swagger from 'fastify-swagger';
 import { app } from './app';
 import config from './common/config';
 
-const server = fastify({ logger: true });
+const logger = pino({
+  // transport: {
+  //   target: 'pino-pretty',
+  //   options: {
+  //     colorize: true,
+  //   },
+  // },
+  transport: {
+    targets: [
+      {
+        level: 'info',
+        target: 'pino-pretty',
+        options: {
+          colorize: true,
+        },
+      },
+      {
+        level: 'warn',
+        target: 'pino/file',
+        options: { destination: './logs.txt' },
+      },
+    ],
+  },
+});
+
+const server = fastify({ logger });
+
+const handlerError = (error: unknown): void => {
+  server.log.error(error);
+  process.exit(1);
+};
 
 server.register<FastifyPluginOptions>(swagger, {
   exposeRoute: true,
@@ -18,9 +49,12 @@ server.register(app);
 const start = async () => {
   try {
     await server.listen(config.PORT);
+
+    // throw Error('Oops!');
   } catch (err) {
-    server.log.error(err);
-    process.exit(1);
+    // server.log.error(err);
+    // process.exit(1);
+    handlerError(err);
   }
 };
 start();
