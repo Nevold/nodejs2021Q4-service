fa2a7e66bf9e6c850bbcc30718b8b3fb67fa0989
Nevold <Nevold@tut.by>
2022-02-05 15:08:51 +0300

    feat: add artillery test

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
   app.useLogger(app.get(Logger));
-  app.flushLogs();
   await app.listen(process.env.PORT, process.env.HTTP_ADDRESS, () =>
     console.log(`Server started on port:${process.env.PORT}`),
   );
